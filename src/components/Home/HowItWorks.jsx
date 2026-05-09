a3dbf673e15420eb7ce1119199fb5bb9da110f33
Aanshik Sharma <aanshik16@gmail.com>
2026-05-09 13:51:52 +0530

    Semanticism Fixes

--- src/components/Home/HowItWorks.jsx
+++ src/components/Home/HowItWorks.jsx
@@ -10,44 +10,42 @@ function HowItWorks() {
   ];
 
   return (
-    <section className="">
-      <div className="max-w-5xl mx-auto px-6 py-12 space-y-16">
-        <article className="space-y-4">
-          <h2 className="uppercase text-2xl mb-12">how it works?</h2>
+    <section className="max-w-5xl mx-auto px-6 py-12 space-y-16">
+      <article className="space-y-4">
+        <h2 className="uppercase text-2xl mb-12">how it works?</h2>
 
-          <p className="text-muted">
-            Enter the SGPA for each semester and the calculator computes your
-            CGPA instantly using AKTU’s official grading formula.
-          </p>
+        <p className="text-muted">
+          Enter the SGPA for each semester and the calculator computes your CGPA
+          instantly using AKTU’s official grading formula.
+        </p>
 
-          <p className="text-muted">
-            AKTU calculates CGPA using a weighted average of the SGPA from each
-            semester, where each semester’s contribution depends on its total
-            credit hours.
-          </p>
-        </article>
+        <p className="text-muted">
+          AKTU calculates CGPA using a weighted average of the SGPA from each
+          semester, where each semester’s contribution depends on its total
+          credit hours.
+        </p>
+      </article>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 items-stretch gap-6">
-          {features.map((feature) => (
-            <Card
-              key={feature.id}
-              body={feature.body}
-              colorVariant={feature.id % 2 !== 0 ? "accentLight" : "accentDark"}
-            />
-          ))}
-        </div>
-
-        <Link
-          to="/about#cgpa-calculation"
-          className="w-fit text-muted hover:text-secondary transition duration-300 flex items-center gap-1 group"
-        >
-          Learn more about calculating CGPA{" "}
-          <BsArrowRightShort
-            size={24}
-            className="group-hover:translate-x-2 transition duration-300"
+      <div className="grid grid-cols-1 md:grid-cols-3 items-stretch gap-6">
+        {features.map((feature) => (
+          <Card
+            key={feature.id}
+            body={feature.body}
+            colorVariant={feature.id % 2 !== 0 ? "accentLight" : "accentDark"}
           />
-        </Link>
+        ))}
       </div>
+
+      <Link
+        to="/about#cgpa-calculation"
+        className="w-fit text-muted hover:text-secondary transition duration-300 flex items-center gap-1 group"
+      >
+        Learn more about calculating CGPA{" "}
+        <BsArrowRightShort
+          size={24}
+          className="group-hover:translate-x-2 transition duration-300"
+        />
+      </Link>
     </section>
   );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,15 +13,17 @@ function About() {
     <main className="max-w-5xl mx-auto">
       <div className="flex">
         <div className="space-y-24 px-6 py-16">
-          {aboutPageData.map((block) => (
+          {aboutPageData.map((block, index) => (
             <section
               key={block.id}
               id={block.id}
               className="space-y-12 scroll-mt-25"
             >
-              <h2 className="uppercase font-semibold text-primary text-2xl">
-                {block.heading}
-              </h2>
+              {index === 0 ? (
+                <h1 className="uppercase text-4xl">{block.heading}</h1>
+              ) : (
+                <h2 className="uppercase text-2xl">{block.heading}</h2>
+              )}
 
               <div className="grid gap-8">{RenderContent(block.content)}</div>
             </section>
